99eaf3438211a894e295e825b7a1df80f390ebb9
lowLevelLucifer <uthkarsh75@gmail.com>
2026-04-12 20:07:42 +0530

    added failed attempts

--- src/scoring.c
+++ src/scoring.c
@@ -6,14 +6,25 @@ float score_time_of_day(int64_t timestamp_unix){
 	struct tm* tm_info = localtime(&t);
 	int hour = tm_info->tm_hour;
 	if(hour>= 9 && hour<= 18){
-		return 0.0;
+		return 0.0f;
 	}if(hour>= 18 && hour<= 22){
-                return 0.3;
-        }if(hour>= 22 && hour<= 6){
-                return 1.0;
+                return 0.3f;
+        }if(hour>= 22 || hour<= 6){
+                return 1.0f;
         }if(hour>= 6 && hour<= 9){
-                return 0.5;
+                return 0.5f;
         }
 	return 0.5f;
 }
-
+float score_failed_attempts(uint8_t failed_attempts){
+	if(failed_attempts > 3){
+		return 1.0f;	
+	}if(failed_attempts == 3){
+		return 0.5f;
+	}if(failed_attempts == 2){
+		return 0.3f;
+	}if(failed_attempts == 1){
+		return 0.2f;
+	}
+	return 0.0f;
+}
